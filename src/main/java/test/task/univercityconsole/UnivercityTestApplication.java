d0d6f68df3abbd112952613229befa6c625f9391
sltnsrh <lifenjoy8888@gmail.com>
2022-05-13 13:47:34 +0300

    first commit

---
+++ src/main/java/test/task/univercityconsole/UnivercityTestApplication.java
@@ -0,0 +1,13 @@
+package test.task.univercityconsole;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnivercityTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnivercityTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/test/task/univercityconsole/UnivercityTestApplicationTests.java
@@ -0,0 +1,13 @@
+package test.task.univercityconsole;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnivercityTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
