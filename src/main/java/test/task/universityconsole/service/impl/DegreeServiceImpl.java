a3e8b6f9c7a002ede42fc3f3c7bb1e31e975d6e9
sltnsrh <lifenjoy8888@gmail.com>
2022-05-16 12:18:53 +0300

    redundant methods were deleted from degree service

--- src/main/java/test/task/universityconsole/service/impl/DegreeServiceImpl.java
+++ src/main/java/test/task/universityconsole/service/impl/DegreeServiceImpl.java
@@ -1,6 +1,5 @@
 package test.task.universityconsole.service.impl;
 
-import java.util.List;
 import org.springframework.stereotype.Service;
 import test.task.universityconsole.model.Degree;
 import test.task.universityconsole.repository.DegreeRepository;
@@ -17,16 +16,5 @@ public class DegreeServiceImpl implements DegreeService {
     @Override
     public Degree add(Degree degree) {
         return degreeRepository.save(degree);
-    }
-
-    @Override
-    public Degree getByName(String name) {
-        return degreeRepository
-                .getDegreeByDegreeName(Degree.DegreeName.valueOf(name.toUpperCase()));
-    }
-
-    @Override
-    public List<Degree> getAll() {
-        return degreeRepository.getAll();
     }
 }
